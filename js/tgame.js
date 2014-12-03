fe6b221b70a9543666b0088433147ac34776bb57
buseca <ruggero.motta@gmail.com>
2014-12-03 22:00:30 +0100

    new commit

--- js/tgame.js
+++ js/tgame.js
@@ -8,11 +8,24 @@ $(document).ready(function() {
   // touchmap________
   // ----------------------
 
-var 
-//ritpetere azione in loop timer
+var rugge = $('.planet.rugge');
+var den = $('.planet.den');
+var planet = $('.planet');
+
+var ruggeNav = rugge.find('span').text();
+var denNav = den.find('span').text();
+
+
+
+faccio il mining delle navicelle sui pianeti base
 setInterval(function() {
-     $()
+  ++ ruggeNav;
+  ++ denNav;
+  rugge.find('span').text(ruggeNav);
+  den.find('span').text(denNav);
 }, 10000);
+
+
 
 
 });
