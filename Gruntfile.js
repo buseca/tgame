ca88bab1996fe3b426228df306ba76f809e5c736
buseca <ruggero.motta@gmail.com>
2014-12-05 16:23:34 +0100

    gruntfile added
    tasks: autoprefixer, watch (live reload), connect, sass

---
+++ Gruntfile.js
@@ -0,0 +1,56 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {                              
+		dist: {                            
+			options: {                       
+				style: 'expanded'
+			},
+			files: {                         
+				'css/tgame.css': 'scss/tgame.scss'       
+			}
+		}
+    },
+	autoprefixer: {
+	      options: {
+	        browsers: ['last 2 version', 'ie 8', 'ie 9']
+	      },
+	      all: {
+	        src: ['css/tgame.css']
+	      }
+	},
+    watch: {
+		files: ['scss/tgame.scss'],
+		tasks: ['sass', 'autoprefixer'],
+		livereload: {
+			files: ['scss/tgame.scss'],
+			options: {
+				livereload: true
+			}
+		}
+    },
+    connect: {
+    	tgame: {
+    		options: {
+    			port: 9000,
+    			base: '<%= public %>',
+    			open: true,
+    			livereload: true,
+    			hostname: 'localhost'
+    		}
+    	}
+    },
+
+  });
+
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-autoprefixer');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+
+	grunt.registerTask('default', ['connect:tgame','watch']);
+
+};
+
+
