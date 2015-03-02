775b928d6405c1ebdc95111df975894974163089
andrewnotari <andrea.notari@econverso.it>
2015-03-03 00:19:49 +0100

    aggiunti i colori

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -19,18 +19,56 @@ angular.module('tGameApp')
 
     //console.log(list.gameData.planetOnStage);
     //console.log(list );
+    $scope.players = [];
 
     list.$loaded().then(function() {
       
+      _.forEach(list.gameData.players, function(player, key) {
+        $scope.players.push(player);
+        // console.log(_.find(list.gameData.players, { list.gameData.players.email: planet.email }));
+      });
 
 
       _.forEach(list.roundData, function(planet, key) {
+        // console.log(_.find($scope.players, { $scope.players.email: planet.email }));
+        // console.log(_.find($scope.players, { $scope.players.email: planet.email }));
+
+        var playerOwner = _.find($scope.players, function (player) {
+          return player.email === planet.owner;
+        });
+        if (playerOwner) {
+          planet.color = playerOwner.color;
+        } else {
+          planet.color = "white"
+        }
         $scope[key] = planet;
-        console.log($scope[key]);
-
-        //console.log(_.find(list.gameData.players, { list.gameData.players.email: planet.email }));
+        // $scope[key].push(playerOwner);
       });
-
+      var movingShips = null,
+          movingShipsOwner = null;
+      $scope.moveShipFromPlanet = function (planet) {
+        if (movingShips == null) {
+          movingShips = planet.navs;
+          movingShipsOwner = planet.owner;
+          console.log(movingShips);
+            
+        } else {
+          if (planet.owner != movingShipsOwner) {
+            
+            if ((movingShips - planet.navs) > 0) {
+              planet.owner = movingShipsOwner;
+            } else if ((movingShips - planet.navs) == 0) {
+              planet.owner = "neutro";
+            }
+            planet.navs = Math.abs(movingShips - planet.navs);
+          } else {
+            planet.navs = movingShips + planet.navs;
+          }
+          movingShips = null;
+          movingShipsOwner = null;
+        }
+        
+      }
 
 
 
