91bb2eb7843db15660ec8528db1c802db861c32f
dbertella <daniele.bertella@gmail.com>
2015-03-02 19:27:57 +0100

    prendo i dati dal db

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -15,41 +15,14 @@ angular.module('tGameApp')
   	// oggetto json con tutti i dati scricati al loading della pagina:
 	// /// oggetto json con dati dei players:
 	// console.log(allData.)
-	var list = sync.$asArray();
-    list.$loaded().then(function() {
-      console.log("list has " + list.length + " item(s)");
-      $scope.activePlayer = $getRecord(0);
-    });
-// place the list into $scope for use in the DOM
+	var list = sync.$asObject();
 
+  //console.log(list.gameData.planetOnStage);
+  //console.log(list );
 
-
-
- //    // we can add it directly to $scope if we want to access this from the DOM
-    $scope.data = list;
-
-
- // var obj = $firebase(new Firebase(url)).$asObject();
-
- //     // to take an action after the data loads, use the $loaded() promise
- //     obj.$loaded().then(function() {
- //        console.log("loaded record:", obj.$id, obj.someOtherKeyInData);
-
- //       // To iterate the key/value pairs of the object, use `angular.forEach()`
- //       angular.forEach(obj, function(value, key) {
-         
-
- // 		  console.log(value);
-
- //       });
- //     });
-
- //     // To make the data available in the DOM, assign it to $scope
- //     // $scope.data = allData.activePlayer;
-
- //     console.log(obj("activePlayer"));
-     // For three-way data bindings, bind it to the scope instead
-     //obj.$bindTo($scope, "data");
-
+  list.$loaded().then(function() {
+    $scope.roundData = list.roundData;
+    console.log($scope.planetsOnStage);
+  });
 	  
   });
